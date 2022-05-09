56992cb8a121b5bfa976e3c718a6a211406d3a35
Diego Oliveira <35296262+diogoditorr@users.noreply.github.com>
2022-05-09 18:49:54 -0300

    NLW Return – Mission Impulse: Stage 2/5

---
+++ web/src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import { CircleNotch } from "phosphor-react";
+import React from "react";
+
+export function Loading() {
+    return (
+        <div className="w-6 h-6 flex items-center justify-center overflow-hidden">
+            <CircleNotch weight="bold" className="h-4 w-4 animate-spin" />
+        </div>
+    );
+}
